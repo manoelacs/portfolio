88766b1f44d0777962dc5b166413ca6a5332f92b
manoela <manoelacs@ic.ufal.br>
2024-11-30 21:33:53 +0100

    Implement theme context and toggle functionality; refactor App layout and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import './App.css';
-import Home from './components/home';
-import About from './components/about';
-import Projects from './components/projects';
-import Contact from './components/contact';
-import Footer from './components/footer';
+
+import Header from './molecules/header';
+import { Navbar } from './molecules/navbar';
+import { SideMenu } from './molecules/sideMenu';
+import { PageSection } from './molecules/pageSection';
+import { Footer } from './atoms/footer';
 
 const App: React.FC = () => {
   return (
-    <div className='font-sans'>
-      <Home />
-      <About />
-      <Projects />
-      <Contact />
+    <div className='flex flex-col items-center h-full gap-y-4 w-[1230px] bg-background dark:bg-dark-background'>
+      <Header />
+      <div className='flex flex-row gap-x-4 justify-end items-end'>
+        <SideMenu />
+        <div className='flex flex-col gap-y-4 h-full justify-end items-end'>
+          <Navbar />
+          <PageSection />
+        </div>
+      </div>
       <Footer />
     </div>
   );
