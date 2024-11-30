85783056fc35272295c90d9d0d87f5657227b770
manoela <manoelacs@ic.ufal.br>
2024-11-30 21:41:17 +0100

    Update Vite config to change alias from '@' to '@src'

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   plugins: [react(), svgr()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@src': path.resolve(__dirname, './src'),
     },
   },
   server: {
