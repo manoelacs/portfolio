e6a9bfe86db44eaca5c852fc47a2213b3e7295a6
manoela <manoelacs@ic.ufal.br>
2024-11-30 21:34:38 +0100

    Add About and Footer components

---
+++ src/atoms/footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Footer: React.FC = () => {
+  return (
+    <footer className='py-5 items-center gap-2.5 flex justify-center'>
+      <span className='text-text dark:text-dark-text font-normal font-poppins'>
+        &copy; {new Date().getFullYear()} Manoela.
+      </span>
+      <span className="text-accent l font-normal font-['Pacifico']">
+        All rights reserved.
+      </span>
+    </footer>
+  );
+};
